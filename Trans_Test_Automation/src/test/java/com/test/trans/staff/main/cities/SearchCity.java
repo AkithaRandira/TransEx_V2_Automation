1a44bc17866ddae06682349d083f2ffafcc26934
AkithaRandira <akitharandira@gmail.com>
2024-11-07 22:45:37 +0530

    staff feature

--- Trans_Test_Automation/src/test/java/com/test/trans/staff/main/cities/SearchCity.java
+++ Trans_Test_Automation/src/test/java/com/test/trans/staff/main/cities/SearchCity.java
@@ -1,4 +1,20 @@
 package com.test.trans.staff.main.cities;
 
-public class SearchCity {
+import com.test.trans.staff.base.BaseTest;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SearchCity extends BaseTest {
+
+    @Test(priority = 1)
+    public void createCityTest() {
+        // Call the login function from BaseTest
+        login();
+    }
 }
